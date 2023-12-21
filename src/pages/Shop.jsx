54d0060f7e50c530fe1fcb3b90c4124fc1227729
Alisher <your_email@whatever.com>
2023-12-21 22:55:10 +0600

    make header
    сделал шапку магазина начну работу с карточками

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,44 +1,52 @@
-// import React from 'react'
-// import styles from "../assets/styles/shop.module.css"
-// import ReactCarousel from "react-bootstrap/Carousel";
-// // import Search from "react-router/dom";
-// // import Form from "react-form";
-// import carImg from "../assets/img/bmwM4.png"
-// import shose from  "../assets/img/nike.png"
+import React from 'react'
+import styles from "../assets/styles/shop.module.css"
+import carImg from "../assets/img/bmwM4.png"
+import shoes from  "../assets/img/nike.png"
+import ReactCarousel from "react-bootstrap/Carousel";
+import basket from "../assets/img/shopping.png"
 
 
-
-// class App extends React.Component {
-//   state = {
-//     items: [
-//       {
-//         title: "BMW M4 Coupe",
-//         price: 23000,
-        
-//       },
-//       {
-//         title: "Adidas Yeezy 500",
-//         price: 100,
-//         image: {shose},
-//       },
-//     ],
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Main page</h1>
-//         <ReactCarousel>
-//           {this.state.items.map((item) => (
-//             <div key={item.title}>
-//               <img src={item.image} alt={item.title} />
-//               <h2>{item.title}</h2>
-//               <p>{item.price}</p>
-//             </div>
-//           ))}
-//         </ReactCarousel>
-//       </div>
-//     );
-//   }
-// }
-// export default App;
+class App extends React.Component {
+    state = {
+      items: [
+        {
+          title: "BMW M4 Coupe",
+          price: 23000,
+          image: {carImg},
+        },
+        {
+          title: "Adidas Yeezy 500",
+          price: 100,
+          image: {shoes},
+        },
+      ],
+    };
+  
+    render() {
+      return (
+        <div>
+           <div className={styles.header}>
+          <div className={styles.logo}>
+          <img className={styles.basketImg} src={basket} alt="" /> <br />
+          <h1 className={styles.txt}>MOBI MARKET</h1>
+          </div>
+          <div className={styles.auth}>
+            <button className={styles.btn}>Подать объявление</button>
+          </div>
+           </div>
+          <ReactCarousel>
+            {this.state.items.map((item) => (
+              <div key={item.title}>
+                <img src={item.image} alt={item.title} />
+                <h2>{item.title}</h2>
+                <p>{item.price}</p>
+              </div>
+            ))}
+          </ReactCarousel>
+        </div>
+      );
+    }
+  }
+  
+  export default App;
+ 
