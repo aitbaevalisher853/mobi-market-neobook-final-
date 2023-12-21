ab58804ce03a669685731391f79643f181acc7be
Alisher <your_email@whatever.com>
2023-12-21 16:51:46 +0600

    stopt
    остановил деятельность магазина так как я еще не продумал до конца

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import styles from "../assets/styles/shop.module.css"
-import ReactCarousel from "react-bootstrap/Carousel";
-// import Search from "react-router/dom";
-// import Form from "react-form";
-import carImg from "../assets/img/bmwM4.png"
-import shose from  "../assets/img/nike.png"
+// import React from 'react'
+// import styles from "../assets/styles/shop.module.css"
+// import ReactCarousel from "react-bootstrap/Carousel";
+// // import Search from "react-router/dom";
+// // import Form from "react-form";
+// import carImg from "../assets/img/bmwM4.png"
+// import shose from  "../assets/img/nike.png"
 
 
 
-class App extends React.Component {
-  state = {
-    items: [
-      {
-        title: "BMW M4 Coupe",
-        price: 23000,
+// class App extends React.Component {
+//   state = {
+//     items: [
+//       {
+//         title: "BMW M4 Coupe",
+//         price: 23000,
         
-      },
-      {
-        title: "Adidas Yeezy 500",
-        price: 100,
-        image: {shose},
-      },
-    ],
-  };
+//       },
+//       {
+//         title: "Adidas Yeezy 500",
+//         price: 100,
+//         image: {shose},
+//       },
+//     ],
+//   };
 
-  render() {
-    return (
-      <div>
-        <h1>Main page</h1>
-        <ReactCarousel>
-          {this.state.items.map((item) => (
-            <div key={item.title}>
-              <img src={item.image} alt={item.title} />
-              <h2>{item.title}</h2>
-              <p>{item.price}</p>
-            </div>
-          ))}
-        </ReactCarousel>
-      </div>
-    );
-  }
-}
-export default App;
+//   render() {
+//     return (
+//       <div>
+//         <h1>Main page</h1>
+//         <ReactCarousel>
+//           {this.state.items.map((item) => (
+//             <div key={item.title}>
+//               <img src={item.image} alt={item.title} />
+//               <h2>{item.title}</h2>
+//               <p>{item.price}</p>
+//             </div>
+//           ))}
+//         </ReactCarousel>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
