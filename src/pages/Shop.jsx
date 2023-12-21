5b9049364c44866e9238d789b184b89bd74cdd82
Alisher <your_email@whatever.com>
2023-12-21 23:10:13 +0600

    delete react bootstrab carousel

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,26 +2,11 @@ import React from 'react'
 import styles from "../assets/styles/shop.module.css"
 import carImg from "../assets/img/bmwM4.png"
 import shoes from  "../assets/img/nike.png"
-import ReactCarousel from "react-bootstrap/Carousel";
 import basket from "../assets/img/shopping.png"
 
 
 class App extends React.Component {
-    state = {
-      items: [
-        {
-          title: "BMW M4 Coupe",
-          price: 23000,
-          image: {carImg},
-        },
-        {
-          title: "Adidas Yeezy 500",
-          price: 100,
-          image: {shoes},
-        },
-      ],
-    };
-  
+   
     render() {
       return (
         <div>
@@ -34,15 +19,7 @@ class App extends React.Component {
             <button className={styles.btn}>Подать объявление</button>
           </div>
            </div>
-          <ReactCarousel>
-            {this.state.items.map((item) => (
-              <div key={item.title}>
-                <img src={item.image} alt={item.title} />
-                <h2>{item.title}</h2>
-                <p>{item.price}</p>
-              </div>
-            ))}
-          </ReactCarousel>
+
         </div>
       );
     }
