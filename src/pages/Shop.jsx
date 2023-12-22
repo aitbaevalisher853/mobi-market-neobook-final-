401ecb831784230522f97840e7c8099ce8db32f4
Alisher <your_email@whatever.com>
2023-12-22 22:16:32 +0600

    начал делать карточки и в принципе получается

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,10 +4,14 @@ import carImg from "../assets/img/bmwM4.png"
 import shoes from  "../assets/img/nike.png"
 import basket from "../assets/img/shopping.png"
 import avatar from "../assets/img/avatar.png"
+import disLike from "../assets/img/Heart.png"
+import like from "../assets/img/HeartRad.png"
+
+
 
 
 class App extends React.Component {
-   
+
     render() {
       return (
         <div>
@@ -26,6 +30,48 @@ class App extends React.Component {
                 <img className={styles.avatar} src={avatar} alt="" />
             </div>
           </div>
+           </div>
+           <div className={styles.cards }>
+           <div className={styles.card}>
+      <img className={styles.car} src={carImg} alt="" />
+      <h2>BMW M4 Coupe: A </h2>
+      <p>Two-Door</p>
+      <p>23 000$</p>
+      <div className={styles.rating}>
+        <img src={disLike} alt="" />
+        <span>100</span>
+      </div>
+    </div>
+    <div className={styles.card}>
+      <img className={styles.car} src={carImg} alt="" />
+      <h2>BMW M4 Coupe: A </h2>
+      <p>Two-Door</p>
+      <p>23 000$</p>
+      <div className={styles.rating}>
+      <img src={like} alt="" />
+        <span>100</span>
+      </div>
+    </div>
+    <div className={styles.card}>
+      <img className={styles.car} src={carImg} alt="" />
+      <h2>BMW M4 Coupe: A </h2>
+      <p>Two-Door</p>
+      <p>23 000$</p>
+      <div className={styles.rating}>
+      <img src={like} alt="" />
+        <span>100</span>
+      </div>
+    </div>
+    <div className={styles.card}>
+      <img className={styles.car} src={carImg} alt="" />
+      <h2>BMW M4 Coupe: A </h2>
+      <p>Two-Door</p>
+      <p>23 000$</p>
+      <div className={styles.rating}>
+      <img src={like} alt="" />
+        <span>100</span>
+      </div>
+    </div>
            </div>
         </div>
       );
